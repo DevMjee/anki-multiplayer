853e0345df95eb0e2f00032d0dc9afa6672ec0d1
DevMjee <dev.mjee2@gmail.com>
2026-02-22 15:10:37 +0000

    adding soup, using it to extract html from raw cards

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,17 @@
 """Python module to read from Anki deck and extract set of questions and answers and store them"""
+from bs4 import BeautifulSoup as bs
 
 
 def extract(src: str) -> dict[str, str]:
     """Function reads deck txt file and outputs questions and correct answers"""
-    ...
+    # read text file
+    with open(src) as file:
+        cards = file.read()
+
+    # create html soup object from raw text to easily search contents
+    card_soup = bs(cards, 'html.parser')
+
+    print(card_soup)
 
 
 def transform(mark_scheme: dict[str, str]) -> dict[str, set[str]]:
@@ -12,9 +20,10 @@ def transform(mark_scheme: dict[str, str]) -> dict[str, set[str]]:
 
 
 def load(qs_and_as: dict[str, set[str]], dest: str) -> None:
-    """Function saves question and answer dictionary to destination filepath"""
+    """Function saves question and answers dictionary to destination filepath"""
     ...
 
 
 if __name__ == '__main__':
-    ...
+    src = 'data/Master Quiz 26 Sept.txt'  # assume source is downloaded as txt file
+    extract(src)
